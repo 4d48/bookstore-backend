67fa193ae9d5098b81e0701715a0884bd9773ae9
x4d47 <x4d47@proton.me>
2026-08-08 19:06:59 +0300

    Update ORM models for books

--- app/api/books/models.py
+++ app/api/books/models.py
@@ -1,11 +1,31 @@
 from decimal import Decimal
 
-from sqlalchemy import Numeric, String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Column, ForeignKey, Integer, Numeric, String, Table
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(DeclarativeBase):
     pass
+
+
+books_to_authors = Table(
+    "books_to_authors",
+    Base.metadata,
+    Column(
+        "book_id",
+        Integer,
+        ForeignKey("books.id", ondelete="CASCADE"),
+        primary_key=True,
+        nullable=False,
+    ),
+    Column(
+        "author_id",
+        Integer,
+        ForeignKey("authors.id", ondelete="CASCADE"),
+        primary_key=True,
+        nullable=False,
+    ),
+)
 
 
 class Book(Base):
@@ -18,3 +38,20 @@ class Book(Base):
     isbn: Mapped[str] = mapped_column(String(20), nullable=True, unique=True)
     price: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=True)
     currency: Mapped[str] = mapped_column(String(3), nullable=True)
+
+    authors: Mapped[list["Author"]] = relationship(
+        secondary=books_to_authors, back_populates="books"
+    )
+
+
+class Author(Base):
+    __tablename__: str = "authors"
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True, autoincrement=True, nullable=False
+    )
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    books: Mapped[list["Book"]] = relationship(
+        secondary=books_to_authors, back_populates="authors"
+    )
